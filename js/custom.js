506dac0ea9b9beac39edc3ade10682fb1765adb6
sonali-agnext <sonali@agnext.in>
2022-06-29 14:37:44 +0530

    Update script and add image for band logo

--- js/custom.js
+++ js/custom.js
@@ -26,35 +26,90 @@ function inputSocial(){
     document.getElementById('mob_area').innerHTML = html;
     
 }
+// watermark(['https://file.removal.ai/preview/716ff022-d983-4f2c-b38d-0b4f50dd0d3c_shutterstock_648907024.png', 'image/download.png'])
+//   .image(watermark.image.upperLeft(0.5))
+//   .then(function (img) {
+//     document.getElementById('upper-left').appendChild(img);
+//   });
+async function run() {
+    const imgUrl =
+      "https://file.removal.ai/preview/716ff022-d983-4f2c-b38d-0b4f50dd0d3c_shutterstock_648907024.png";
+    //1. Convert the image path to canvas
+    const tempCanvas = await imgToCanvas(imgUrl);
+    //2. Add watermark to canvas
+    const canvas = addWatermark(tempCanvas, "https://agnext.com/wp-content/uploads/2021/03/cropped-logo-grad.png");
+    //3. Convert canvas to img
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+    // const img =       
+    //   fetch("https://quickchart.io/watermark?mainImageUrl=https://file.removal.ai/preview/55b0a2fb-a0e8-49bd-ac60-2fd962bb62b4_shutterstock_648907024.png&markImageUrl=https://agnext.com/wp-content/uploads/2021/03/cropped-logo-grad.png&markRatio=0.25", requestOptions)
+    //     .then(response => response.text())
+    //     .then(function(result){
+    //         console.log(result);
+    //     })
+    //     .catch(error => console.log('error', error));;
+        const imageUrl = "https://quickchart.io/watermark?mainImageUrl=https://file.removal.ai/preview/55b0a2fb-a0e8-49bd-ac60-2fd962bb62b4_shutterstock_648907024.png&markImageUrl=https://agnext.com/wp-content/uploads/2021/03/cropped-logo-grad.png&markRatio=0.25";
+
+        (async () => {
+          const response = await fetch(imageUrl)
+          const imageBlob = await response.blob()
+          const reader = new FileReader();
+          reader.readAsDataURL(imageBlob);
+          reader.onloadend = () => {
+            const base64data = reader.result;
+            const img = document.createElement("img");
+            img.src = base64data;
+            img.setAttribute ("crossorigin", "anonymous");
+            document.body.appendChild((img));
+          }
+        })()
+    //View effects
+    
+}
+run();
+
+async function imgToCanvas(url) {
+    //Create img element
+    const img = document.createElement("img");
+    img.src = url;
+    img.setAttribute ("crossorigin", "anonymous"); // prevent failed to execute 'todataurl' on 'htmlcanvas element' caused by cross domain: tainted canvas may not be exported
+    await new Promise((resolve) => (img.onload = resolve));
+    //Create the canvas DOM element and set its width and height to be the same as the picture
+    const canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    //The coordinates (0,0) indicate that the drawing starts from here and is equivalent to an offset.
+    canvas.getContext("2d").drawImage(img, 0, 0);
+    return canvas;
+}
+
+function addWatermark(canvas, text) {
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "red";
+    ctx.textBaseline = "middle";
+    ctx.fillText('', 20, 20);
+    return canvas;
+}
+
+  function convasToImg(canvas) {
+    //Create a new image object, which can be understood as dom
+    var image = new Image();
+    // canvas.toDataURL  It returns a string of Base64 encoded URLs
+    //Specified format png
+    image.src = canvas.toDataURL("image/png");
+    image.className = 'watermarked';
+    return image;
+  }
+
 function showPreview(event){		
     if(event.target.files.length > 0){
-        var data = new FormData();
-        data.append("image_file", event.target.files[0]);
-        // data.append("image_url", "url_to_image");
-
-        // var xhr = new XMLHttpRequest();
-        // xhr.withCredentials = true;
-
-        // xhr.addEventListener("readystatechange", function() {
-        //     if(this.readyState === 4) {
-        //         console.log(this.responseText);
-        //     }
-        // });
-
-        // xhr.open("POST", "https://api.remove.bg/v1.0");
-        // xhr.setRequestHeader("X-Api-Key", "cHAPmXFUNCYG4QXwS4MYNed6");
-        // xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-        // xhr.setRequestHeader('Access-Control-Allow-Credentials', true);
-
-        // xhr.send(data);
-
         var myHeaders = new Headers();
         myHeaders.append("Rm-Token", "62bacdd957e371.06369301");
-        myHeaders.append("Cookie", ".AspNetCore.Session=CfDJ8HIfJWbHxklAhwC3OsvlqYRaQn%2FCLCWGZjoEuGd23g%2BYZZzQ92F4jBdJ1%2BD4G2CCYxGgHO%2F1BjPPmazmJMzo9fezd3Qfg8%2BIqlghdiV38jN2Sfi4iDn0%2Bx3fBk%2FT%2Br14DTiWAGvk2WAqCatTOB8znxzyWiMH%2FxBFqLNxzka56I9%2F");
         
         var formdata = new FormData();
-        console.log(fileInput.files[0]);
-        formdata.append("image_file", fileInput.files[0], "shutterstock_648907024.jpg");
+        formdata.append("image_file", event.target.files[0], "shutterstock_648907024.jpg");
         formdata.append("image_url", "url_to_image");
         
         var requestOptions = {
@@ -63,10 +118,13 @@ function showPreview(event){
           body: formdata,
           redirect: 'follow'
         };
-        
+
         fetch("https://api.removal.ai/3.0/remove", requestOptions)
           .then(response => response.text())
-          .then(result => console.log(result))
+          .then(function(result){
+            console.log(result);
+            
+          })
           .catch(error => console.log('error', error));
 
 
